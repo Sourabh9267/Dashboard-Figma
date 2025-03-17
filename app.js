1aa31346cad669bb6e3009f5e90d490431337ef7
Sourabh Gour <saurabhgour2222@gmail.com>
2025-03-17 17:52:21 +0530

    Layout Fixed + Toggler width adjusted

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var sidebar = document.querySelector("#sidebar-wrapper-div");
 
+var page=document.querySelector("#page");
+
+console.log(page);
+
+
 console.log(sidebar);
 
 var isClicked = false;
@@ -10,10 +15,15 @@ function togglesidebar() {
     if (isClicked) {
     
         sidebar.style.width="0px";
+        page.style.width="100%";
+        page.style.marginLeft="0";
+        
         console.log("Sidebar Hidden");
     } else {
         console.log("Sidebar visible");
-  
+        
+        page.style.marginLeft="260px";
+        page.style.width="calc(100% - 260px)";
         sidebar.style.width="260px";
     }
 }
